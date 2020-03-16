4f29c337d102ee82479d7ed6d35e8fc6de860bd1
SureDaddy <51438198+SureDaddy@users.noreply.github.com>
2020-03-16 17:59:03 +0800

    UI Start

---
+++ Assets/Scripts/ElectricSheep.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ElectricSheep : MonoBehaviour
+{
+    bool deactivating = false;
+    // Update is called once per frame
+    void Update()
+    {
+        StateChecker();
+    }
+
+    void SetBubbleActive()
+    {
+        gameObject.SetActive(false);
+        deactivating = false;
+    }
+
+
+    void StateChecker()
+    {
+        if(gameObject.activeInHierarchy == true && !deactivating)
+        {
+            Invoke("SetBubbleActive", 0.5f);
+            deactivating = true;
+        }
+    }
+}
