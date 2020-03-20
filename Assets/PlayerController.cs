e6418f46d974b6b60d73209787fdd7c3ac0968a6
John Jackson <madcow@westnet.com.au>
2020-03-20 09:24:58 +0800

    Yeah Cool Man
    nicola is big meanie

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject selectedGroup;
     public Joystick playerJoystick;
+    public Camera playerCamera;
 
     // Start is called before the first frame update
     void Start()
@@ -62,7 +63,6 @@ public class PlayerController : MonoBehaviour
         }
         //move group
         selectedGroup.transform.Translate(inputDirection * Time.deltaTime, Space.World);
-
     }
 
     void JoystickTest()
@@ -86,6 +86,25 @@ public class PlayerController : MonoBehaviour
         for(int nn = 0; nn < selectedGroup.GetComponent<GenericGroup>().npcs.Length; nn++)
         {
             selectedGroup.GetComponent<GenericGroup>().npcs[nn].GetComponent<NPC_Controller>().myCurrentAnim = (NPC_Controller.animationOptions)moveDirection;
+        }
+    }
+
+    void GroupSelection()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            selectedGroup = null;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit2D hitData = Physics2D.Raycast(playerCamera.ScreenToWorldPoint(Input.mousePosition), Vector3.forward, 100);
+            if(hitData == true)
+            {
+                if (hitData.collider.gameObject.CompareTag("Group"))
+                {
+                    selectedGroup = hitData.collider.gameObject;
+                }
+            }
         }
     }
 
